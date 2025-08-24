546f5f5949f14b37c40a90c6fcff99452b471683
Yan Karpinskyi <yankarp377@gmail.com>
2025-08-25 00:02:02 +0300

    Added files for production - v.1.0

---
+++ universal-calc-app/src/img/imgImports.js
@@ -0,0 +1,19 @@
+import plus from './plus.png';
+import minus from './minus.png';
+import multiple from './multiple.png';
+import divide from './divide.png';
+import celcium from './celcium.png';
+import cm from './cm.png';
+import usd from './usd.png';
+import eur from './eur.png';
+
+export const images = {
+  plus,
+  minus,
+  multiple,
+  divide,
+  celcium,
+  cm,
+  usd,
+  eur
+};
