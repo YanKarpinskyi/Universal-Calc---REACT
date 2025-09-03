5e6adb5130c452ddfb5b2a3123a2456227245ddc
YanKarpinskyi <yankarp377@gmail.com>
2025-09-03 09:34:53 +0300

    Added vercel.json file and .gitignore, replace README file

--- universal-calc-app/src/reportWebVitals.js
+++ universal-calc-app/src/reportWebVitals.js
@@ -1,10 +1,11 @@
-// src/reportWebVitals.js
-const reportWebVitals = (onPerfEntry) => {
+const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getLCP }) => {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
+      getFCP(onPerfEntry);
       getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
     });
   }
 };
